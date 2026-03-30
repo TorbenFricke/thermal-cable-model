eec545961df2207e3827f4831556558d3d1c0538
Torben Fricke <mail@torben.co>
2026-03-30 11:09:45 +0200

    rename thermal FEM to thermal-cable-model

--- thermal_fem/fem/__init__.py
+++
@@ -1,5 +0,0 @@
-"""2-D finite-element sub-package for soil temperature field computation."""
-
-from thermal_fem.fem.mesh import RectangularMesh
-from thermal_fem.fem.elements import assemble_system
-from thermal_fem.fem.solver import FEMSolver
